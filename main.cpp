0cfd95490b9cb53633d894d9ff17cd83626aaa0e
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-01-02 07:59:59 +0200

    Added search button, made background black and added icons

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //Set the text of the entire application to white
+    a.setStyleSheet(
+        "QWidget {"
+        "   color: white;"  // Set text color to white
+        "   background-color: #2E2E2E;"  // Optional: Set a dark background
+        "}"
+        );
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,23 +7,52 @@ MainWindow::MainWindow(QWidget *parent)
 
     QWidget *centralWidget = new QWidget(this);
     setCentralWidget(centralWidget);
+    centralWidget->setStyleSheet("background-color: #2E2E2E;");
 
     QWidget *sidebar = new QWidget(this);
     sidebar->setFixedWidth(250);
-
     QVBoxLayout *sidebarLayout = new QVBoxLayout(sidebar);
 
-    QLabel *sidebarTitle = new QLabel("Menu",sidebar);
-    sidebarTitle->setAlignment(Qt::AlignCenter);
+    QWidget *searchWidget = new QWidget(sidebar);
+    QHBoxLayout *searchLayout = new QHBoxLayout(searchWidget);
+    QLineEdit *searchInput = new QLineEdit(searchWidget);
+    searchInput->setPlaceholderText("Search");
+    searchInput->setFixedWidth(250);
+    searchInput->setFixedHeight(30);
+    searchLayout->addWidget(searchInput);
+    searchWidget->setLayout(searchLayout);
 
     QListWidget *menuList = new QListWidget(sidebar);
-    menuList->addItem("My Day");
-    menuList->addItem("Important");
-    menuList->addItem("Tasks");
 
-    QPushButton *logoutButton = new QPushButton("Logout",sidebar);
+    QListWidgetItem *myDayItem = new QListWidgetItem("My Day");
+    myDayItem->setIcon(QIcon("C:/Users/realm/OneDrive/Documents/QtProjects/todolist2-without-ui/icons/sun icon.png"));
 
-    sidebarLayout->addWidget(sidebarTitle);
+    QListWidgetItem *importantItem = new QListWidgetItem("Important");
+    importantItem->setIcon(QIcon("C:/Users/realm/OneDrive/Documents/QtProjects/todolist2-without-ui/icons/star icon.png"));
+
+    QListWidgetItem *tasksItem = new QListWidgetItem("Tasks");
+    tasksItem->setIcon(QIcon(":/icons/home icon.jpg"));
+
+    QListWidgetItem *groceryItem = new QListWidgetItem("Groceries");
+    groceryItem->setIcon(QIcon(":/icons/shopping icon.png"));
+
+    menuList->addItem(myDayItem);
+    menuList->addItem(importantItem);
+    menuList->addItem(tasksItem);
+    menuList->addItem(groceryItem);
+
+    menuList->setStyleSheet(
+        "QListWidget::item {"
+        "   height: 20px;"  // Adjust height
+        "   padding: 10px;" // Add padding
+        "   font-size: 16px;"
+        "   color: white;"
+        "}"
+        );
+
+    QPushButton *logoutButton = new QPushButton("Exit",sidebar);
+
+    sidebarLayout->addWidget(searchWidget);
     sidebarLayout->addWidget(menuList);
     sidebarLayout->addWidget(logoutButton);
 
@@ -42,11 +71,28 @@ MainWindow::MainWindow(QWidget *parent)
 
     QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
     mainLayout->addWidget(splitter);
+
+    connect(menuList, &QListWidget::currentRowChanged, [contentLabel](int currentRow) {
+        switch (currentRow) {
+        case 0:
+            contentLabel->setText("This is the 'My Day' section");
+            break;
+        case 1:
+            contentLabel->setText("This is the 'Important' section.");
+            break;
+        case 2:
+            contentLabel->setText("This is the 'Tasks' section.");
+            break;
+        default:
+            contentLabel->setText("Main Content Area");
+            break;
+        }
+    });
 }
 
 QSize MainWindow::sizeHint() const
 {
-    return QSize(2000,1500);
+    return QSize(900,850);
 }
 
 MainWindow::~MainWindow()
